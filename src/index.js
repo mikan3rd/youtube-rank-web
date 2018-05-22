f4d0f0f8f408ebd5b56126043eb48ea1ef3efece
mikan3rd <mikan.no.kikoudhi@gmail.com>
2018-05-22 19:24:49 +0900

    feat: react-gaの実装

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
-import '../stylesheets/style.scss';
-
 import {IndexRoute, Route, Router, browserHistory} from 'react-router';
-
-import ConfigureStore from './helpers/store';
-import Index from './containers/Index';
 import {Provider} from 'react-redux';
 import React from 'react';
 import {render} from 'react-dom';
+import ReactGA from 'react-ga';
 
+import configureStore from './helpers/store';
+import Index from './containers/Index';
 import registerServiceWorker from './registerServiceWorker';
 
 import 'onsenui/css/onsenui.css';
 import 'onsenui/css/onsen-css-components.css';
 
+import '../stylesheets/style.scss';
 import '../stylesheets/00_foundation/foundation.css';
 import '../stylesheets/00_foundation/reset.css';
 import '../stylesheets/00_foundation/loading.css';
 
-const store = ConfigureStore();
+
+const store = configureStore();
+
+ReactGA.initialize('UA-119666839-1', {debug: true});
+const page = window.location.href;
+console.log("page:", page)
+ReactGA.pageview(page);
 
 render(
   (
